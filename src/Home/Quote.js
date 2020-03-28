5594ec9571b69164a297fc0319a63e424fbb848b
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-03-28 16:44:56 -0500

    add in random motivational quote

--- src/Home/Quote.js
+++ src/Home/Quote.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './Home.css'
+const RandQuote = require('inspirational-quotes');
 
 const Quote = () => {
     return(
         <div>
             <div className="home-header">
-                <h1>Motivational Quote</h1>
+                <h1>{RandQuote.getRandomQuote()}</h1>
             </div>
         </div>
     );
