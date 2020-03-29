c8d122f507321a9da704619e36545a6dc49b54fd
SaloniSS <sxs170015@utdallas.edu>
2020-03-28 22:46:06 -0500

    Fun Stuff

--- src/FunStuff/FunStuff.js
+++ src/FunStuff/FunStuff.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './FunStuff.css'
+
+const funStuff = [
+    "https://media.giphy.com/media/Z5SMZDDtlPgL6/giphy.gif",
+    "https://media.giphy.com/media/lqVQEskOmoopO/giphy.gif",
+    "https://media.giphy.com/media/10xUg8DdgQSs9i/giphy.gif",
+    "https://media.giphy.com/media/VkMV9TldsPd28/giphy.gif",
+     "https://media.giphy.com/media/7beSjLq7J6qfS/giphy.gif",
+     "https://media.giphy.com/media/ZBoHqyxmhv85ff3qOI/giphy.gif",
+     "https://media.giphy.com/media/ZBoHqyxmhv85ff3qOI/giphy.gif",
+     "https://media.giphy.com/media/PnvOB6zsNb04g/giphy.gif",
+     "https://media.giphy.com/media/PnvOB6zsNb04g/giphy.gif",
+     "https://media.giphy.com/media/gxxlowyvtfS0M/giphy.gif"
+];
+
 const FunStuff = () => {
+    const randomIndex = Math.floor(Math.random() * funStuff.length); 
     return(
         <div>
-            <h1>Fun Stuff</h1>
+            <div className="intro-bar">
+                <h1>Fun Stuff</h1>
+            </div>
+            <div className="intro-desc">
+                <h1>These Cutest Collection</h1>
+            </div>
+            <img className="fun" src={funStuff[randomIndex]} />
         </div>
     );
 };
