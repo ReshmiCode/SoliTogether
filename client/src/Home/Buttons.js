83650403b4441c27f13b1995f6a38e6db5e425cf
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-03-28 21:05:21 -0500

    change nav way

--- client/src/Home/Buttons.js
+++ client/src/Home/Buttons.js
@@ -3,18 +3,12 @@ import { Link } from 'react-router-dom';
 
 import './Home.css'
 
-const Buttons = () => {
+const Buttons = props => {
     return(
         <div>
-            <Link to="/home-tasks">
-                <button className="button">Tasks</button>
-            </Link>
-            <Link to="/home-resources">
-                <button className="button">Resources</button>
-            </Link>
-            <Link to="/">
-                <button className="button">Fun Stuff</button>
-            </Link>
+            <button className="button" onClick={() => props.changeTab('home-tasks')}>Tasks</button>
+            <button className="button" onClick={() => props.changeTab('home-resources')}>Resources</button>
+            <button className="button" onClick={() => props.changeTab('/')}>Fun Stuff</button>
         </div>
     );
 };
