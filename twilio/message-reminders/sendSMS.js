2a57322c872cf35709fc5da3e67946b33583e99e
meganjtran <meganjtran@gmail.com>
2020-03-29 11:38:17 -0500

    added to messages

--- twilio/message-reminders/sendSMS.js
+++ twilio/message-reminders/sendSMS.js
@@ -4,14 +4,21 @@ require('dotenv').config()
 
 const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
-let messageArray = ["mei", "saloni", "reshmi", "vincent", "pinguino"];
-
+let messageArray = 
+    ["Make sure you stay hydrated!", 
+    "Make sure you're getting some fresh air!", 
+    "Stay healthy, wash your hands for at least 20 seconds!", 
+    "Stay home to keep yourself and others healthy!", 
+    "Stay strong, we will get through this together!", 
+    "Times are tough, but you are tougher!", 
+    "Eat some fruits and veggies every day!", 
+    "Don't forget to exercise every day! Stay active!"];
 
 client.messages
   .create({
      body: messageArray[Math.floor(Math.random() * messageArray.length)],
      from: process.env.MY_NUMBER,
-     to: "+15056158722"
+     to: "+14693804692"
    })
   .then(message => { console.log(message.sid); })
   .catch(err => console.error(err));
